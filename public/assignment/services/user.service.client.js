65f822a39b8936d701b6dd030eaeecf30e93fa3b
CherylRuo <dione1183348@gmail.com>
2016-10-24 13:41:19 -0700

    changed login info

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -8,7 +8,7 @@
     function UserService() {
         var users = [
             {_id: 123, username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: 234, username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+            {_id: 234, username: "cheryl",      password: "cheryl",      firstName: "Cheryl",    lastName: "Jia"  },
             {_id: 345, username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
             {_id: 456, username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
         ];
